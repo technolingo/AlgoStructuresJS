77c159f7955be0001f521055755bf78deee98e11
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-10 15:30:27 +0900

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,46 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar (str) {
+//   var charMap = {};
+//   var maxChar = '';
+//   var maxCount = 0;
+//
+//   for (let char of str) {
+//     if (char in charMap) {
+//       charMap[char] += 1;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+//
+//   for (let char in charMap) {
+//     if (charMap[char] > maxCount) {
+//       maxChar = char;
+//       maxCount = charMap[char];
+//     }
+//   }
+//
+//   return maxChar;
+// }
+
+const maxChar = str => {
+  const charMap = {};
+  let maxChar = '';
+  let maxCount = 0;
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > maxCount) {
+      maxChar = char;
+      maxCount = charMap[char];
+    }
+  }
+
+  return maxChar;
+};
 
 module.exports = maxChar;
