9e5c0b8a117f22e19e998af545c85eebd363ea0b
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-10 17:56:46 +0900

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams (stringA, stringB) {
+  const first = stringA.replace(/[^a-z]/gi, '').toLowerCase().split('').sort().join('');
+  const second = stringB.replace(/[^a-z]/gi, '').toLowerCase().split('').sort().join('');
+  return first === second;
+}
+
+// const charMap = string => {
+//   const obj = {};
+//   const strippedString = string.replace(/[^a-z]/gi, '').toLowerCase();
+//   for (let char of strippedString) {
+//     obj[char] = obj[char] + 1 || 1;
+//   }
+//   return obj;
+// };
+//
+// const anagrams = (stringA, stringB) => {
+//   const charMapA = charMap(stringA);
+//   const charMapB = charMap(stringB);
+//
+//   if (Object.keys(charMapA).sort().join('') !== Object.keys(charMapB).sort().join('')) {
+//     return false;
+//   }
+//
+//   for (let char in charMapA) {
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// };
 
 module.exports = anagrams;
