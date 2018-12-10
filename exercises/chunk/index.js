dd768954b694126f0938a3bbc2885bc5fbbafeac
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-10 16:50:54 +0900

    array chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk (array, size) {
+//   var chunked = [];
+//   for (let i of array) {
+//     if (chunked.slice(-1)[0] == null || chunked.slice(-1)[0].length === size) {
+//       chunked.push([i]);
+//     } else {
+//       chunked.slice(-1)[0].push(i);
+//     }
+//   }
+//   return chunked;
+// }
+
+// const chunk = (array, size) => {
+//   const chunked = [];
+//   for (let i of array) {
+//     const lastChunk = chunked[chunked.length - 1];
+//     if (!lastChunk || lastChunk.length === size) {
+//       chunked.push([i]);
+//     } else {
+//       lastChunk.push(i);
+//     }
+//   }
+//   return chunked;
+// };
+
+const chunk = (array, size) => {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index = index + size;
+  }
+  return chunked;
+};
 
 module.exports = chunk;
