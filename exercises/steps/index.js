54bab317f938af2fa7e31474a522996ce4333949
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-12 14:27:12 +0900

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,46 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+const steps = n => {
+  for (let i = 1; i <= n; i++) {
+    console.log('#' * i + ' ' * (n - i));
+  }
+};
+
+// const steps = n => {
+//   for (let row = 1; row <= n; row++) {
+//     let stair = '';
+//     for (let col = 1; col <= n; col++) {
+//       if (col <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// };
+
+// const steps = (n, row = 0, stair = '') => {
+//   if (row === n) {
+//     return;
+//   }
+//
+//   // recurse at the end of every row
+//   if (stair.length === n) {
+//     console.log(stair);
+//     steps(n, row + 1); // only change row
+//     return;
+//   }
+//
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//
+//   // recurse through every row
+//   steps(n, row, stair); // only change stair
+// };
 
 module.exports = steps;
