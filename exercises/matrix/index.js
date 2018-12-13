753509109b820374edd0d6a525ff3dabe35b833e
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-13 16:20:47 +0900

    spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -10,11 +10,50 @@
 //     [8, 9, 4],
 //     [7, 6, 5]]
 //  matrix(4)
-//     [[1,   2,  3, 4],
+//     [[1,  2,  3, 4],
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+const matrix = n => {
+  const mat = [];
+  for (let i = 0; i < n; i++) {
+    mat.push([]);
+  }
+
+  let counter = 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  while (startRow <= endRow && startCol <= endCol) {
+    // top current row
+    for (let i = startCol; i <= endCol; i++) {
+      mat[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    // rightmost col top-down
+    for (let i = startRow; i <= endRow; i++) {
+      mat[i][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+    // bottom row right-left
+    for (let i = endCol; i >= startCol; i--) {
+      mat[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    // leftmost col down-top
+    for (let i = endRow; i >= startRow; i--) {
+      mat[i][startCol] = counter;
+      counter++;
+    }
+    startCol++;
+  }
+
+  return mat;
+};
 
 module.exports = matrix;
