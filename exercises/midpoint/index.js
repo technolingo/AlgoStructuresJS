9c0dfb15c2d446eb0041c07dff99547a565b0aa7
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-16 16:52:27 +0900

    find midpoint in a linked list in javascript

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -2,9 +2,9 @@
 // Return the 'middle' node of a linked list.
 // If the list has an even number of elements, return
 // the node at the end of the first half of the list.
-// *Do not* use a counter variable, *do not* retrieve
-// the size of the list, and only iterate
-// through the list one time.
+// * Do not use a counter variable,
+// * do not retrieve the size of the list,
+// * and only iterate through the list one time.
 // --- Example
 //   const l = new LinkedList();
 //   l.insertLast('a')
@@ -12,6 +12,33 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+const midpoint = list => {
+  // if (!list.head) {
+  //   return null;
+  // } else if (!list.head.next || !list.head.next.next) {
+  //   // when the list has 1 or 2 elements
+  //   return list.head;
+  // }
+  // // when the list has more than 2 elements
+  // let slow = list.head;
+  // let fast = list.head;
+  // for (let node of list) {
+  //   slow = node;
+  //   fast = fast.next.next;
+  //   if (!fast.next || !fast.next.next) {
+  //     return slow.next;
+  //   }
+  // }
+  if (!list.getFirst()) {
+    return null;
+  }
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
 
 module.exports = midpoint;
--- exercises/midpoint/test.js
+++ exercises/midpoint/test.js
@@ -7,7 +7,20 @@ test('Midpoint is a function', () => {
   expect(typeof midpoint).toEqual('function');
 });
 
+describe('Midpoint returns null when the list is empty', () => {
+  test('return null when the list is empy', () => {
+    const l = new LinkedList();
+    expect(midpoint(l)).toEqual(null);
+  });
+});
+
 describe('Midpoint returns the middle node of an odd numbered list', () => {
+  test('when the list has 1 element', () => {
+    const l = new LinkedList();
+    l.insertLast('a');
+    expect(midpoint(l).data).toEqual('a');
+  });
+
   test('when the list has 3 elements', () => {
     const l = new LinkedList();
     l.insertLast('a');
@@ -24,6 +37,20 @@ describe('Midpoint returns the middle node of an odd numbered list', () => {
     l.insertLast('d');
     l.insertLast('e');
     expect(midpoint(l).data).toEqual('c');
+  });
+
+  test('when the list has 9 elements', () => {
+    const l = new LinkedList();
+    l.insertLast('a');
+    l.insertLast('b');
+    l.insertLast('c');
+    l.insertLast('d');
+    l.insertLast('e');
+    l.insertLast('f');
+    l.insertLast('g');
+    l.insertLast('h');
+    l.insertLast('i');
+    expect(midpoint(l).data).toEqual('e');
   });
 });
 
@@ -42,5 +69,18 @@ describe('Midpoint returns the middle node of an even numbered list', () => {
     l.insertLast('c');
     l.insertLast('d');
     expect(midpoint(l).data).toEqual('b');
+  });
+
+  test('when the list has 8 elements', () => {
+    const l = new LinkedList();
+    l.insertLast('a');
+    l.insertLast('b');
+    l.insertLast('c');
+    l.insertLast('d');
+    l.insertLast('e');
+    l.insertLast('f');
+    l.insertLast('g');
+    l.insertLast('h');
+    expect(midpoint(l).data).toEqual('d');
   });
 });
