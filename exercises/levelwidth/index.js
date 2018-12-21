77d437305d846115fd4af28b1b1b7032588ee049
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-21 17:27:46 +0900

    find each level's width of a tree in javascript

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -10,7 +10,24 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+const Node = require('./node');
 
-function levelWidth(root) {}
+const levelWidth = root => {
+  const counters = [0];
+  const nodes = [root, 'x']; // use 'x' to seperate each level
+
+  while (nodes.length > 1) {
+    const node = nodes.shift();
+    if (node === 'x') {
+      nodes.push('x');
+      counters.push(0);
+    } else {
+      counters[counters.length - 1]++;
+      nodes.push(...node.children);
+    }
+  }
+
+  return counters;
+};
 
 module.exports = levelWidth;
