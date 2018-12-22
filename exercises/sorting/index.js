2f960ae04b346bb56a527889d9935e32b1458516
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-22 18:05:46 +0900

    mergesort implemented in javascript

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -33,12 +33,30 @@ const selectionSort = arr => {
   return arr;
 };
 
-const mergeSort =arr => {
-
-};
-
 const merge = (left, right) => {
+  const merged = [];
 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      merged.push(left.shift());
+    } else {
+      merged.push(right.shift());
+    }
+  }
+
+  return [...merged, ...left, ...right];
 };
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+const mergeSort = arr => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const centre = Math.floor(arr.length / 2);
+  const left = arr.slice(0, centre);
+  const right = arr.slice(centre);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
