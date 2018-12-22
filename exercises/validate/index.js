af99dd0dcdf072f5fe42b6a4cd684b89746c7b8d
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-22 15:55:50 +0900

    validate a binary search tree in javascript

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -1,10 +1,24 @@
 // --- Directions
-// Given a node, validate the binary search tree,
+// Given a root node, validate the binary search tree,
 // ensuring that every node's left hand child is
 // less than the parent node's value, and that
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+const validate = (node, min = null, max = null) => {
+  if (node !== null) {
+    if (max !== null && node.data >= max) {
+      return false;
+    } else if (min !== null && node.data <= min) {
+      return false;
+    } else if (node.left !== null) {
+      return validate(node.left, min, node.data);
+    } else if (node.right !== null) {
+      return validate(node.right, node.data, max);
+    }
+    return true;
+  }
+  return false;
+};
 
 module.exports = validate;
