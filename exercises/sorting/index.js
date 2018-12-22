40d68a005f0374376c0931ba8ab9fb0bf23793b8
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-22 16:58:15 +0900

    bubblesort implementation in javascript

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,30 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+const bubbleSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        const current = arr[j];
+        const next = arr[j + 1];
+        arr[j] = next;
+        arr[j + 1] = current;
+      }
+    }
+  }
+  return arr;
+};
 
-}
+const selectionSort = arr => {
 
-function selectionSort(arr) {
+};
 
-}
+const mergeSort =arr => {
 
-function mergeSort(arr) {
+};
 
-}
+const merge = (left, right) => {
 
-function merge(left, right) {
-
-}
+};
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
