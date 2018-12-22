b0a5a41e26dde7f66d181aa263c63fe321af8a2c
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-22 17:15:19 +0900

    selection sort implementation in javascript

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,21 @@ const bubbleSort = arr => {
 };
 
 const selectionSort = arr => {
-
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      const min = arr[indexOfMin];
+      const current = arr[i];
+      arr[i] = min;
+      arr[indexOfMin] = current;
+    }
+  }
+  return arr;
 };
 
 const mergeSort =arr => {
